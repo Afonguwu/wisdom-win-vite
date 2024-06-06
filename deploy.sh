bd3cc57679fe368d81c951cbc215f51ad0656701
Afong <t225310917@gmail.com>
2024-06-06 14:55:24 +0800

    upgrate

--- deploy.sh
+++ deploy.sh
@@ -8,5 +8,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push  -f git@github.com:Afonguwu/wisdom-win-vite.git main:gh-pages
+git push  -f git@github.com:Afonguwu/wisdom-win-vite.git master:gh-pages
 cd ..
