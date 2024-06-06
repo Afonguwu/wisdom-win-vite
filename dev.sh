42fbf482cefe1db975fbb7c379e398a277124193
Afong <t225310917@gmail.com>
2024-06-06 13:15:18 +0800

    sh data

---
+++ dev.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+# Push the changes in the main branch to the main branch
+git init
+git add -A
+git commit -m ''
+git push git@github.com:Afonguwu/wisdom-win.vite.git master:main
