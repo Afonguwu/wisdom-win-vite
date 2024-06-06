3ab2eb266d3fce7691e2b0589b5b64c4ffad1f26
Afong <t225310917@gmail.com>
2024-06-06 14:39:11 +0800

    upgrate

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ npm run build
 git init
 git add .
 git commit -m ' upgrate'
-git push git@github.com:Afonguwu/wisdom-win-vite.git main:main
+git push -f git@github.com:Afonguwu/wisdom-win-vite.git main:main
 
 git add dist
 git commit -m 'deploy'
