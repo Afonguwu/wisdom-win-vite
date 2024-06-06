eb6aff5fd9c52632cd4d461c012acef4ab33e6ce
Afong <t225310917@gmail.com>
2024-06-06 14:45:37 +0800

    upgrate

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,9 @@ npm run build
 git init
 git add .
 git commit -m ' upgrate'
-git push -f git@github.com:Afonguwu/wisdom-win-vite.git main:main
+git push git@github.com:Afonguwu/wisdom-win-vite.git main:main
 
-git add dist
+git add dist/.
 git commit -m 'deploy'
-git push  -f git@github.com:Afonguwu/wisdom-win-vite.git main:gh-pages
+git push  git@github.com:Afonguwu/wisdom-win-vite.git main:gh-pages
 cd ..
