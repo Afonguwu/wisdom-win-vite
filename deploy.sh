64f66c51fbb434b635688ca8f040a9497903e9a7
Afong <t225310917@gmail.com>
2024-06-06 14:19:06 +0800

    upgrate

--- deploy.sh
+++ deploy.sh
@@ -3,3 +3,10 @@ git init
 git add .
 git commit -m ' upgrate'
 git push git@github.com:Afonguwu/wisdom-win-vite.git main:main
+
+cd dist
+git init
+git add -A
+git commit -m 'deploy'
+git push -f git@github.com:Afonguwu/wisdom-win-vite.git main:gh-pages
+cd ..
