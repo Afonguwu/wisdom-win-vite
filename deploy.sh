f8dd7b052b42a1154d5313eab1fc97244276b6e2
Afong <t225310917@gmail.com>
2024-06-16 13:40:53 +0800

    refactor: 💡 update deploy.sh command (add git-cz)

--- deploy.sh
+++ deploy.sh
@@ -2,12 +2,12 @@
 npm run build
 git init
 git add .
-git commit -m ' router update'
+git cz
 git push git@github.com:Afonguwu/wisdom-win-vite.git main:main
 #deploy to gh-pages
 cd dist
 git init
 git add -A
-git commit -m 'deploy'
+git cz
 git push  -f git@github.com:Afonguwu/wisdom-win-vite.git master:gh-pages
 cd ..
